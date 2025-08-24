8a74dd9dcfc658a0ed8bb2fdb92dac2841e65971
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2025-08-24 11:28:28 -0300

    chore: config automatic topic creation in services

--- orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/enums/ETopics.java
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/enums/ETopics.java
@@ -1,4 +1,4 @@
-package br.com.microservices.orchestrated.orchestratorservice;
+package br.com.microservices.orchestrated.orchestratorservice.core.enums;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,10 +13,10 @@ public enum ETopics {
     FINISH_FAIL("finish-fail"),
     PRODUCT_VALIDATION_SUCCESS("product-validation-success"),
     PRODUCT_VALIDATION_FAIL("product-validation-fail"),
-    PAYMENT_VALIDATION_SUCCESS("payment-validation-success"),
-    PAYMENT_VALIDATION_FAIL("payment-validation-fail"),
-    INVENTORY_VALIDATION_SUCCESS("inventory-validation-success"),
-    INVENTORY_VALIDATION_FAIL("inventory-validation-fail"),
+    PAYMENT_SUCCESS("payment-success"),
+    PAYMENT_FAIL("payment-fail"),
+    INVENTORY_SUCCESS("inventory-success"),
+    INVENTORY_FAIL("inventory-fail"),
     NOTIFY_ENDING("notify-ending");
 
     private final String topic;
